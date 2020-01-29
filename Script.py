37c54c1bfe51b909320f48970a9db26fed19b9c5
AymericNoel <aymeric.noel@edu.devinci.fr>
2020-01-29 16:45:28 +0100

    quesrtion 1

--- Script.py
+++ Script.py
@@ -1,1 +1,15 @@
-print("sous gros test")
+import json
+import requests
+
+url="http://api.binance.com/api/v3/exchangeInfo"
+key = "KegqmLKeO6HiVeV4yue6Kt1AIucusHAHBex2MAqoMF3OqOttb5NUxgm3znuONHIT"
+response=requests.get(url)
+response_json = response.json()
+
+allCrypto=[]
+for i in range(0,len(response_json['symbols'])):
+    if(response_json['symbols'][i]['baseAsset'] not in allCrypto):
+        allCrypto.append(response_json['symbols'][i]['baseAsset'])
+
+
+print(allCrypto)
